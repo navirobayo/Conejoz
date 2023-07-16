c09aef114713057ab2e9fc8f26a982d7573a0683
Ivan Robayo <robayoivan2077@gmail.com>
2023-07-16 15:15:47 -0500

    Theme Service Creation

---
+++ conejoz/lib/components/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ConejozTheme {
+  ConejozTheme._();
+
+  static const Color _lightPrimaryColor = Colors.lime;
+
+  static const Color _darkPrimaryColor = Colors.indigo;
+
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+        seedColor: ConejozTheme._lightPrimaryColor,
+        brightness: Brightness.light),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+        seedColor: ConejozTheme._darkPrimaryColor, brightness: Brightness.dark),
+  );
+}
---
+++ conejoz/lib/components/theme_service.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeService extends ChangeNotifier {
+  bool isDarkMode = false;
+
+  void switchTheme() {
+    isDarkMode = !isDarkMode;
+    notifyListeners();
+  }
+}
