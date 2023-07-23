8473c35aa67870d376b604b5cccb5d95930e7f80
Ivan Robayo <robayoivan2077@gmail.com>
2023-07-23 02:27:44 -0500

    Profile screen draft

--- conejoz/lib/presentation/profile_screen/profile_screen.dart
+++ conejoz/lib/presentation/profile_screen/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+double regularSpacer = 25;
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
@@ -8,24 +10,62 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("About"),
+        title: const Text("Dreamerspace"),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(children: [
+        SizedBox(height: regularSpacer),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text(
-              'This is the profile screen',
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/101908819?v=4'),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            Text(
-              'An Open Source App!',
-            ),
-            Text(
-              'github.com/navirobayo',
-            ),
+            Text('John Doe', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
-      ),
+        SizedBox(height: regularSpacer),
+        const Text(
+            "A quote about dreaming. The user can select it from a selection available for the users, or can create one."),
+        Card(
+          clipBehavior: Clip.hardEdge,
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {
+              // Open dream
+            },
+            child: const SizedBox(
+              width: 150,
+              height: 150,
+              child: Center(child: Text("Journal 1")),
+            ),
+          ),
+        ),
+        SizedBox(height: regularSpacer),
+        Card(
+          clipBehavior: Clip.hardEdge,
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {
+              // Open dream
+            },
+            child: const SizedBox(
+              width: 150,
+              height: 150,
+              child: Center(child: Text("Journal 2")),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
