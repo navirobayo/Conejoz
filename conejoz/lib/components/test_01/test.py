d33b2b8d66fab5e8992624fe1eb85765ba6a74ae
Ivan <navirobayo@gmail.com>
2023-07-30 00:11:12 -0500

    Deletion of test

--- conejoz/lib/components/test_01/test.py
+++
@@ -1,7 +0,0 @@
-from dotenv import find_dotenv, load_dotenv
-from transformers import pipeline
-
-load_dotenv(find_dotenv())
-
-# text to image
-text_to_image = pipeline("text2image", model="microsoft/layoutlm-base-uncased")
