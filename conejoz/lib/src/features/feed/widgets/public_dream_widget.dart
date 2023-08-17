7d6af7e72d6abb46cce0733296db036dc4dc6ac4
Ivan <navirobayo@gmail.com>
2023-08-17 03:48:22 -0500

    Public Widger Prototype

--- conejoz/lib/src/features/feed/widgets/public_dream_widget.dart
+++ conejoz/lib/src/features/feed/widgets/public_dream_widget.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
-class PublicDreamWidget extends StatelessWidget {
+class PublicDreamWidget extends StatefulWidget {
   final String rabbit;
   final String dream;
   final String title;
   final String time;
   final String tags;
-  const PublicDreamWidget(
-      {super.key,
-      required this.rabbit,
-      required this.dream,
-      required this.title,
-      required this.time,
-      required this.tags});
+  const PublicDreamWidget({
+    Key? key,
+    required this.rabbit,
+    required this.dream,
+    required this.title,
+    required this.time,
+    required this.tags,
+  }) : super(key: key);
+
+  @override
+  _PublicDreamWidgetState createState() => _PublicDreamWidgetState();
+}
+
+class _PublicDreamWidgetState extends State<PublicDreamWidget> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +35,21 @@ class PublicDreamWidget extends StatelessWidget {
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(rabbit,
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      color: Theme.of(context).colorScheme.onSurface)),
+              Text(
+                widget.rabbit,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
               const Spacer(),
-              Text(time,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(color: Theme.of(context).colorScheme.surface)),
+              Text(
+                widget.time,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: Theme.of(context).colorScheme.surface),
+              ),
             ],
           ),
           const SizedBox(height: 10),
@@ -57,7 +71,7 @@ class PublicDreamWidget extends StatelessWidget {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  title,
+                  widget.title,
                   style: TextStyle(
                     color: Theme.of(context).colorScheme.onBackground,
                     fontSize: Theme.of(context).textTheme.titleMedium?.fontSize,
@@ -66,32 +80,44 @@ class PublicDreamWidget extends StatelessWidget {
                   ),
                 ),
               ),
-              GestureDetector(
-                onLongPress: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Tags'),
-                        content: Text(tags),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text('Close'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  child: Text(tags,
-                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: Theme.of(context).colorScheme.onSecondary)),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    padding: EdgeInsets.zero,
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: Text(
+                    widget.tags,
+                    overflow: TextOverflow.visible,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
                 ),
               ),
+              if (_expanded)
+                PopupMenuButton<String>(
+                  onSelected: (value) {
+                    // Handle tag selection if needed
+                  },
+                  itemBuilder: (BuildContext context) {
+                    return widget.tags.split(', ').map((tag) {
+                      return PopupMenuItem<String>(
+                        value: tag,
+                        child: Text(tag),
+                      );
+                    }).toList();
+                  },
+                ),
             ],
           ),
         ],
